81ad1f51ff76193dfdbd4319bc28d59ae78db55f
Aditya Ramaakrushnan <48169134+adiram359@users.noreply.github.com>
2020-10-14 15:34:53 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ import {
   Route,
 } from 'react-router-dom';
 import { Landing } from './pages/Landing.js';
+import { Browse } from "./pages/Browse.js";
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Landing} />
+      </Switch>
+      <Switch>
+        <Route exact path="/browse" component={Browse}/>
       </Switch>
     </Router>
   );
